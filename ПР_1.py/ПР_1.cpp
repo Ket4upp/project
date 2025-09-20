c42f98d41d2be270ff5aedcd60db9a7e1286512c
Ket4upp <ket4upp888@gmail.com>
2025-09-20 20:30:52 +0300

    Update ПР_1.cpp

--- ПР_1.py/ПР_1.cpp
+++ ПР_1.py/ПР_1.cpp
@@ -1,49 +1,25 @@
 C++ 
 
-  
 #include <iostream>
+#include <list>
 #include <stack>
 #include <string>
 
 int main() {
-  // Создание стека строк
-  std::stack<std::string> myStack;
+  // Создание списка
+  std::list<char> my_list = {'a', 'b', 'c', 'd', 'e', 'f'};
 
-  // Добавление элементов в стек (push)
-  myStack.push("Первый");
-  myStack.push("Второй");
-  myStack.push("Третий");
+  // Организация стека
+  std::stack<char> my_stack;
+  my_stack.push('a');
+  my_stack.push('b');
+  my_stack.push('c');
 
-  // Проверка, пуст ли стек (empty)
-  if (!myStack.empty()) {
-    std::cout << "Стек не пуст." << std::endl;
-  }
-
-  // Получение верхнего элемента стека (top)
-  std::cout << "Верхний элемент стека: " << myStack.top() << std::endl;
-
-  // Удаление верхнего элемента стека (pop)
-  myStack.pop();
-
-  // Получение нового верхнего элемента стека после удаления
-  std::cout << "Новый верхний элемент стека: " << myStack.top() << std::endl;
-
-  // Получение размера стека (size)
-  std::cout << "Размер стека: " << myStack.size() << std::endl;
-
-  // Полное удаление элементов из стека (пока он не станет пустым)
-  while (!myStack.empty()) {
-    std::cout << "Удаляем: " << myStack.top() << std::endl;
-    myStack.pop();
-  }
-
-  // Проверка, пуст ли стек после удаления всех элементов
-  if (myStack.empty()) {
-    std::cout << "Стек пуст." << std::endl;
-  }
+  std::cout << "Верхний элемент стека: " << my_stack.top() << std::endl; // Вывод: c
+  my_stack.pop();
+  std::cout << "Верхний элемент стека: " << my_stack.top() << std::endl; // Вывод: b
 
   return 0;
-}
-
+}  
 
 .cpp
